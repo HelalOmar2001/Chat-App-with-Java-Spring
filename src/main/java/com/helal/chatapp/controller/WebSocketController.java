72cda2e7c3303aba9cc5d03b289f2874a4939bc3
HelalOmar2001 <halahelooo14@gmail.com>
2026-04-29 01:56:03 +0300

    creating simple websocket controller

---
+++ src/main/java/com/helal/chatapp/controller/WebSocketController.java
@@ -0,0 +1,20 @@
+package com.helal.chatapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class WebSocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public String handleMessage(String message){
+        return message;
+    }
+}
